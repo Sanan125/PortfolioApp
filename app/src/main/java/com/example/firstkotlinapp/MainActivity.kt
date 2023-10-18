010f3bbb629431a1467795235f387a3f41719281
sanan-mac <sanan125@yahoo.com>
2023-10-19 01:56:39 +0500

    Main Activity basic layout done.

--- app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.firstkotlinapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSkills = findViewById<Button>(R.id.buttonSkills)
+
+        buttonSkills.setOnClickListener {
+            intent = Intent(this, SkillsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
